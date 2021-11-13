204d8c16e56cc47ba9d92e230219df715cb21904
Aves <carlier.amandine98@gmail.com>
2021-11-13 10:33:09 +0100

    Initialisation du projet
    Création et initialisation de chatoon.

---
+++ chatoon/src/main/java/fr/uphf/technoweb/chatoon/ChatoonApplication.java
@@ -0,0 +1,13 @@
+package fr.uphf.technoweb.chatoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatoonApplication.class, args);
+	}
+
+}
